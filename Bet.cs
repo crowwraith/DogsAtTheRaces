7a7de0d296bace12bc06fb86c89e8e8ba040acc4
crowwraith <1185615@student.roc-nijmegen.nl>
2026-07-03 11:09:58 +0200

    basic setup classes
    starter setup classes

--- Bet.cs
+++ Bet.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DogsAtTheRaces
 {
-    internal class Bet
+    public class Bet
     {
+        public int Amount;
+        public int Dog = 0; // dog number
+        public Guy Bettor; 
+
+        public string GetDescription()
+        {
+            return ("bet"); // moet dalijk Bettor hebben, met een check of amount null is, null = no bet, anders waarde meegeven als bet.en vermelden welke dog het was.
+        }
+
+        public int PayOut(int winner)
+        {
+            if(winner == dog)
+            {
+                return Amount+Amount;
+            }
+        }
     }
 }
